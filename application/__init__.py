13135158565029bf80e19f2d74e4111c98df2595
Cheng-Chang Chou <qq@Q-new-Air.local>
2024-11-06 02:08:54 +0800

    Setup envnt vars for uri @Render

--- application/__init__.py
+++ application/__init__.py
@@ -1,36 +1,13 @@
 # __init__.py
 
-# from flask import Flask
-# from flask_pymongo import PyMongo
-
-# from pymongo.mongo_client import MongoClient
-# from pymongo.server_api import ServerApi
-# uri = "mongodb+srv://q1biblio:DCqsoGKZuOZEwOYd@cluster0.lly3v.mongodb.net/qqdb?retryWrites=true&w=majority&appName=Cluster0"
-# # Create a new client and connect to the server
-# client = MongoClient(uri, server_api=ServerApi('1'))
-
-
-# app = Flask(__name__)
-
-# app.config['SECRET_KEY'] = '8be30bb00390333bb542b482934ed545704f6dae'
-
-# app.config["MONGO_URI"]  ='mongodb+srv://q1biblio:DCqsoGKZuOZEwOYd@cluster0.lly3v.mongodb.net/qqdb?retryWrites=true&w=majority&appName=Cluster0'
-
-# mongo = PyMongo(app)
-# db = mongo.db
-# todos_collection = db.todos_flask
-
-# from application import routes
-
-
-
+import os
 from flask import Flask
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8be30bb00390333bb542b482934ed545704f6dae'
-uri = "mongodb+srv://q1biblio:DCqsoGKZuOZEwOYd@cluster0.lly3v.mongodb.net/qqdb?retryWrites=true&w=majority&appName=Cluster0"
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+uri = os.getenv('uri')
 client = MongoClient(uri, server_api=ServerApi('1'))
 
 # Access the desired database and collection

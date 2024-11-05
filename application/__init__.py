ed7b264b0e178b1bda7f48d44410fe3641c7e80b
Cheng-Chang Chou <qq@Q-new-Air.local>
2024-11-06 01:09:18 +0800

    mongoDB get local connection works

--- application/__init__.py
+++ application/__init__.py
@@ -6,8 +6,8 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '8be30bb00390333bb542b482934ed545704f6dae'
-app.config["MONGO_URI"] = 'mongodb+srv://qq:tGh0v7dvlMmDIvtS@cluster0.pttm2.mongodb.net/cqdatabase?retryWrites=true&w=majority&appName=Cluster0'
 
+app.config["MONGO_URI"]  ='mongodb+srv://q1biblio:DCqsoGKZuOZEwOYd@cluster0.lly3v.mongodb.net/qqdb?retryWrites=true&w=majority&appName=Cluster0'
 
 mongo = PyMongo(app)
 db = mongo.db
--- application/forms.py
+++ application/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, TextAreaField, SelectField, SubmitField
 from wtforms.validators import DataRequired
 
 class TodoForm(FlaskForm):
-    name = SubmitField('Name', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
     description = TextAreaField('Description', validators=[DataRequired()])
     completed = SelectField('Completed', choices = [('False', 'False'), ('True', 'True')], validators=[DataRequired()])
     submit = SubmitField('Add Todo')
